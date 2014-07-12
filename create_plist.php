74e2e5fef231089c3216cd2f9179fdc1cdd08fac
ats <yasuda.atusi@gmail.com>
2014-07-12 17:49:46 +0900

    Create create_plist.php

---
+++ create_plist.php
@@ -0,0 +1,22 @@
+<?php
+$rootNode = new SimpleXMLElement( "<?xml version='1.0' encoding='UTF-8'?>
+    <!DOCTYPE plist PUBLIC '-//Apple//DTD PLIST 1.0//EN' 'http://www.apple.com/DTDs/PropertyList-1.0.dtd'>
+    <plist version='1.0'></plist>" );
+
+$dictNode = $rootNode->addChild('dict');
+
+$dictNode->addChild( 'key', 'Label' );
+$dictNode->addChild( 'string', 'org.miko.osx.envset' );
+$dictNode->addChild( 'key', 'ProgramArguments' );
+
+$keyNode = $dictNode->addChild( 'array' );
+$homedir = getenv('HOME');
+$keyNode->addChild( 'string', $homedir . '/bin/envset' );
+
+$dictNode->addChild( 'key', 'RunAtLoad' );
+$dictNode->addChild( 'true' );
+
+$dom = new DOMDocument( '1.0' );
+$dom->loadXML( $rootNode->asXML() );
+$dom->formatOutput = true;
+echo $dom->saveXML();
